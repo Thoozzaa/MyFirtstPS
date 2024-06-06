bff6c8289fb7711ef90fbadcba91867c041fc48d
Thoozzaa <thoozzaa@gmail.com>
2024-06-06 10:58:14 +0200

    Add files via upload

---
+++ autoinstall.ps1
@@ -0,0 +1,35 @@
+
+$downloadPath = "C:\Downloads"
+
+if (-Not (Test-Path -Path $downloadPath)) {
+    New-Item -Path $downloadPath -ItemType Directory
+}
+function Install-Software {
+    param (
+        [string]$url,
+        [string]$installerName,
+        [string]$arguments
+    )
+
+    $installerPath = "$downloadPath\$installerName"
+
+    Invoke-WebRequest -Uri $url -OutFile $installerPath
+
+    Start-Process -FilePath $installerPath -ArgumentList $arguments -NoNewWindow -Wait
+}
+
+#Chrome
+Install-Software -url "https://dl.google.com/chrome/install/latest/chrome_installer.exe" `
+                 -installerName "chrome_installer.exe" `
+                 -arguments "/silent /install"
+#VLC
+Install-Software -url "https://get.videolan.org/vlc/3.0.11.1/win64/vlc-3.0.11.1-win64.exe" `
+                 -installerName "vlc_installer.exe" `
+                 -arguments "/S"
+
+#7-Zip
+Install-Software -url "https://www.7-zip.org/a/7z1900-x64.exe" `
+                 -installerName "7zip_installer.exe" `
+                 -arguments "/S"
+
+Write-Host "Alle software is geïnstalleerd."
